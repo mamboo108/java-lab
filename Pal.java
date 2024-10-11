658703fce640c836263aa34131da60560fd56133
mamboo108 <147160053+mamboo108@users.noreply.github.com>
2024-10-11 14:23:31 +0530

    Create Pal.java

---
+++ Pal.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Pal
+{
+    public static void main(String args[])
+    {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int num = sc.nextInt();
+        int i=0,rev=0,rem;
+        int temp=num;
+        while(num!=0)
+        {
+            rem=num%10;
+            rev=rev*10+rem;
+            num=num/10;
+        }
+        if(temp==num)
+        {
+            System.out.println("The number is a Palindrome");
+            
+        }
+        else
+        {
+            System.out.println("The number is not a Palindrome");
+        }
+    }
+    
+}
