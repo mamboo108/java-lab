372ec2874285933069052cd517304e1d212af48b
mamboo108 <147160053+mamboo108@users.noreply.github.com>
2024-10-11 15:09:28 +0530

    Create evenodd.java

---
+++ evenodd.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class evenodd
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the value for A: ");
+        int a = sc.nextInt();
+        if(a%2==0)
+        {
+            System.out.println(a+" Is Even");
+        }
+        else
+        {
+            System.out.println(a+" Is Odd");
+        }
+        
+        
+        
+    }
+}
