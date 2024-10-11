b6d9cac4a263710459b52b37db99a54fa46f78cb
mamboo108 <147160053+mamboo108@users.noreply.github.com>
2024-10-11 15:12:58 +0530

    Create fact.java

---
+++ fact.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class factorial
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int num = sc.nextInt();
+        int i,fact=1;
+        for(i=1;i<=num;i++)
+        {
+            fact=fact*i;
+            
+        }
+        System.out.println("The factorial: "+fact);
+        
+        
+    }
+}
